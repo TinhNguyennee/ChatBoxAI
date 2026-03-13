e991e7bafaa3eb0ad903b8239df6973f93e04ac3
Tinh Nguyen <tinhps28445@fpt.edu.vn>
2026-03-13 09:34:13 +0700

    render

--- bot.js
+++ bot.js
@@ -191,6 +191,8 @@ res.send("ok");
 
 
 
-app.listen(3000, ()=>{
- console.log("Webhook server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+ console.log("Server running");
 });
